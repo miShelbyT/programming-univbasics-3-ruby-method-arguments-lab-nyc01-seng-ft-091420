a0dd09768dd37596c34ce6533c323649768cb085
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-23 16:48:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,12 +1,12 @@
 def introduction(name)
   "Hi, my name is #{name}."
 end
-introduction("Shelby")
+puts introduction("Shelby")
 
 def introduction_with_language(name, language)
   "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-introduction_with_language("Shelby", "Ruby")
+puts introduction_with_language("Shelby", "Ruby")
 
 
 
